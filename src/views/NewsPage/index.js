c992b87e13acaa183025645950af8a6989355fcb
9slagger <b5813568@g.sut.ac.th>
2018-12-18 16:22:36 +0700

    add newsPage

---
+++ src/views/NewsPage/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import MainLayout from '../../components/MainLayout'
+
+class NewsPage extends Component {
+  render() {
+    return (
+      <MainLayout>
+        <div className="text-center m-5 p-5">
+          <h1>ข่าวสาร</h1>
+        </div>
+      </MainLayout>
+    )
+  }
+}
+
+export default NewsPage
