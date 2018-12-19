ff8832ef2eb7b45ffc2c9f06161d30ede18aeabb
9slagger <b5813568@g.sut.ac.th>
2018-12-20 01:03:31 +0700

    fix HomeContentActions

--- src/actions/HomeContentActions.js
+++ src/actions/HomeContentActions.js
@@ -2,12 +2,9 @@ import axios from 'axios'
 import { FETCH_HOME_CONTENT } from './type'
 
 export const fetchHomeContent = () => {
-  return async dispatch => {
-    const token = await localStorage.getItem('token')
+  return dispatch => {
     axios
-      .get('https://mytutorapi.herokuapp.com/homecontent', {
-        headers: { Authorization: token }
-      })
+      .get('https://mytutorapi.herokuapp.com/homecontent')
       .then(response => {
         dispatch({ type: FETCH_HOME_CONTENT, payload: response.data })
       })
