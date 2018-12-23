15a8fa0fb4f2d085f02d932e763742bc8dd62147
Polymorph <noonphongnarin@gmail.com>
2018-12-23 22:48:44 +0700

    add PromotionPage

--- src/views/PromotionPage/index.js
+++ src/views/PromotionPage/index.js
@@ -5,8 +5,40 @@ class PromotionPage extends Component {
   render() {
     return (
       <MainLayout>
-        <div className="text-center m-5 p-5">
-          <h1>โปรโมชั่น</h1>
+        <div className="mt-5 px-5 mx-5">
+          <h1 className="mb-3">โปรโมชั่น</h1>
+          <div className="card bg-dark text-white crop2 mb-2">
+            <img
+              className="card-img"
+              src="https://images.unsplash.com/photo-1524059633949-c157729ae351?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+              alt="img photo picture"
+            />
+            <div className="card-img-overlay text_left">
+              <h1 className="card-title">Card title</h1>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
+          </div>
+          <div className="card bg-dark text-white crop2 mb-2">
+            <img
+              className="card-img"
+              src="https://images.unsplash.com/photo-1531303435785-3853ba035cda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+              alt="img photo picture"
+            />
+            <div className="card-img-overlay text_left">
+              <h1 className="card-title">Card title</h1>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">Last updated 3 mins ago</p>
+            </div>
+          </div>
         </div>
       </MainLayout>
     )
