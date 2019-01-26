4d1b3f9a363b0fb692608b375929e2f9fd3f344e
9slagger <akkarapong.kh@gmail.com>
2019-01-27 03:44:10 +0700

    fix PlayVideoPage

--- src/components/UploadFile/index.js
+++ src/components/UploadFile/index.js
@@ -1,58 +1,63 @@
 import React from 'react'
-import { Field, reduxForm } from 'redux-form'
 import axios from 'axios'
 
-const adaptFileEventToValue = delegate => e => delegate(e.target.files[0])
-
-const FileInput = ({
-  input: { value: omitValue, onChange, onBlur, ...inputProps },
-  meta: omitMeta,
-  ...props
-}) => {
-  return (
-    <input
-      onChange={adaptFileEventToValue(onChange)}
-      onBlur={adaptFileEventToValue(onBlur)}
-      type="file"
-      {...props.input}
-      {...props}
-    />
-  )
-}
-
-const FileUpload = props => {
-  const { handleSubmit } = props
-  const onFormSubmit = async data => {
-    const tempdata = await { file: data }
-    // console.log(tempdata)
+class SimpleReactFileUpload extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: 0,
+      file: null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+  }
+  onFormSubmit(e) {
+    e.preventDefault()
+    this.fileUpload(this.state.file)
+  }
+  onChange(e) {
+    this.setState({ file: e.target.files[0] })
+  }
+  fileUpload(file) {
+    const url =
+      'http://35.247.150.186/restricted/lectures?idsec=5c47054b0016120004518761&quality=1080'
+    const formData = new FormData()
+    formData.append('file', file)
+    const config = {
+      onUploadProgress: progressEvent =>
+        this.setState({
+          loading: (progressEvent.loaded / progressEvent.total) * 100
+        }),
+      headers: {
+        'content-type': 'multipart/form-data',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyVHlwZSI6ImFkbWluIiwiZXhwIjoxNTc0NDM4MjA2LCJpZCI6IjVjMTg4NmNiNmZkMzMzN2UxNmQzY2NhZiJ9.qrmz0ZT3P1eYp0dHG26guDw1VwDTFvg7dvEJ9Acy-qw'
+      }
+    }
     axios
-      .post(
-        `http://35.247.150.186/lectures?idsec=5c47054b0016120004518761&quality=1080`,
-        tempdata,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        }
-      )
+      .post(url, formData, config)
       .then(res => {
-        console.log(res.statusText)
+        console.log(res)
       })
       .catch(error => {
         console.log(error)
       })
   }
-  return (
-    <form onSubmit={handleSubmit(onFormSubmit)}>
-      <div>
-        <label>Attachment</label>
-        <Field name="attachment" component={FileInput} type="file" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  )
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange} />
+        <button type="submit">Upload</button>
+        <div>
+          <label>กำลังอัพโหลด</label>
+          <span>{this.state.loading}</span>
+        </div>
+      </form>
+    )
+  }
 }
 
-export default reduxForm({
-  form: 'fileupload'
-})(FileUpload)
+export default SimpleReactFileUpload
--- src/views/PlayVideoPage/index.js
+++ src/views/PlayVideoPage/index.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import plyr from 'plyr'
+// import axios from 'axios'
 
 class PlayVideo extends Component {
   componentDidMount() {
-    const options = {}
+    const options = {
+      toggleInvert: true,
+      seekTime: 10,
+      controls: [
+        'play-large',
+        'play',
+        'progress',
+        'current-time',
+        'mute',
+        'volume',
+        'captions',
+        'settings',
+        'pip',
+        'airplay',
+        'fullscreen'
+      ]
+    }
     this.player = plyr.setup('#plyr-player', options)
   }
   componentWillUnmount() {
@@ -14,8 +31,24 @@ class PlayVideo extends Component {
     }
   }
   render() {
-    const { link } = this.props
-
+    // const { link } = this.props
+    // let data
+    // let token =
+    //   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyVHlwZSI6ImFkbWluIiwiZXhwIjoxNTczNDU3MTUyLCJpZCI6IjVjMTg4NmNiNmZkMzMzN2UxNmQzY2NhZiJ9.iE3q9XoKVDDmHvPtmn_HnOpH8IYCgcr2bjlJjRCcH0o'
+    // axios
+    //   .get(
+    //     `http://35.247.150.186/restricted/video/5c49aaf32bd666748b78aaef-1080.mp4`,
+    //     {
+    //       headers: { Authorization: token }
+    //     }
+    //   )
+    //   .then(res => {
+    //     data = res.data
+    //     console.log(res)
+    //   })
+    //   .catch(error => {
+    //     console.log(error)
+    //   })
     return (
       <div className="text-center">
         <div className="container">
@@ -24,10 +57,11 @@ class PlayVideo extends Component {
             poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
             id="plyr-player"
           >
-            <source src={link} type="video/mp4" size="576" />
-            <source src={link} type="video/mp4" size="720" />
-            <source src={link} type="video/mp4" size="1080" />
-            <source src={link} type="video/mp4" size="1440" />
+            <source
+              src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+              type="video/mp4"
+              size="1080"
+            />
           </video>
         </div>
       </div>
